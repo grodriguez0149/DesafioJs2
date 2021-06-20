733545587160b8bb6f0c64e48da31b38df205d9a
GABRIELA <grodriguez03@hotmail.com>
2021-06-19 22:15:17 -0300

    agregue 2 archivos

---
+++ app.js
@@ -0,0 +1,15 @@
+alert("Podrías completar éste algoritmo? un algoritmo es un conjunto de procedimientos o funciones ordenados que se necesitan para realizar ciertas acciónes");
+alert("Te diremos que edad tiene tu perro en años caninos")
+let nombre = prompt("Ingresa tu nombre");
+let apellido = prompt ("Ingresa tu apellido");
+let edad = parseInt( prompt ("Ingesa tu edad"));
+let perro= parseInt( prompt("Ingresa la edad de tu perro"));
+
+const parametroPerro = 7;
+
+let numeroPerro= parametroPerro * perro;
+
+let usuario = nombre + " " + apellido + " tenes " + edad + " años y tu perro tiene " + numeroPerro;
+
+alert(usuario);
+console.log(usuario);
